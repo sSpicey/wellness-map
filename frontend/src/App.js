f205c6a28c0449367307cd717bb749038878dbf8
sSpicey <martini@alunos.utfpr.edu.br>
2023-04-15 15:16:26 -0300

    Refactor header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { AppBar, Container, Grid, Toolbar, Paper, Typography, Button, IconButton, MenuIcon, ListItem, Slider, Box } from '@mui/material'
 import { useTheme } from '@mui/material/styles';
-import StyledPaper from "./components/StyledPaper";
-import StyledTable from "./components/StyledTable";
+import Header from "./components/Header";
 import SearchIcon from '@mui/icons-material/Search';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -129,13 +128,7 @@ function App() {
 
   return (
     <div className="App" >
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 3 }}>
-            Perfil Estratégico de Exemplo para o Projeto de Bem Estar
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <Header/>
       <Grid item xs={12} sx={{marginTop:'20px'}}>
         <Grid container justifyContent="center" spacing={3}>
           <Grid item>
