a467833d1d92f74f78fe309ede0fa77e3bb5c814
sSpicey <martini@alunos.utfpr.edu.br>
2023-06-28 16:07:15 -0300

    Add navigation link to main menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,23 @@
 import {useState, React}  from 'react'
 import { AppBar, Toolbar, Button} from '@mui/material'
+import { useNavigate } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import imgLogo from "../img/logo-figma1.png"
 import AboutWindow from "./AboutWindow";
 import HelpWindow from "./HelpWindow";
 
 function Header({onAboutClick, onHelpClick, onHamburgerClick, showAbout, showHelp}) {
+  const navigate = useNavigate();
+
+  const onImageClick = () => {
+    navigate('/');
+  }
+
   return (
     <div>
         <AppBar position="static" sx={{ backgroundColor: '#EAEDDC', height: '70px' }}>
           <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <img src={imgLogo} alt="Logo" style={{margin: '5px',height: '60px', objectFit:'contain', marginBottom: '5px', marginLeft: '40px'}}/>
+            <img src={imgLogo} onClick={onImageClick} alt="Logo" style={{margin: '5px',height: '60px', objectFit:'contain', marginBottom: '5px', marginLeft: '40px'}}/>
             <div style={{justifyContent: 'space-evenly', display: 'flex', marginRight: '40px'}}>
               <button 
                 onClick={onAboutClick} 
